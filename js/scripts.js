ce3a9d3e9771a17def1b5bf1bc30b6e3483bd0fc
Sam Kirsch and Tammy Dang <tammy@>
2017-01-11 15:55:10 -0800

    increase the styling on all pages, adding rows and columns as necessary and linking pages together

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
+//Front-end
 $(document).ready(function() {
   $("#info").submit(function(event) {
     (event).preventDefault();
-    var nameInput = $("#name").val();
-    var streetInput = $("#street").val();
-    var cityInput = $("#city").val();
-    var stateInput = $("#state").val();
-    var zipcodeInput= $("#zipcode").val();
+    var genderInput = $("#gender").val();
+    var dessertInput = $("#dessert").val();
     var birthdayInput = $("#birthday").val();
     var colorInput = $("#color").val();
-    var combinedResults = "Name is: " + nameInput;
-    $(".results").text(combinedResults);
+
+    $(".survey-over").show();
+    $(".survey-active").hide();
   });
 
   $("#order-info").submit(function(event) {
@@ -19,13 +18,15 @@ $(document).ready(function() {
     var cityInput = $("#city").val();
     var stateInput = $("#state").val();
     var zipcodeInput= $("#zipcode").val();
-
     var productInput = $("#product").val();
+
     $(".name").text(nameInput);
     $(".street").text(streetInput);
     $(".city").text(cityInput);
     $(".zipcode").text(zipcodeInput);
     $(".product").text(productInput);
 
+    $(".print-receipt").show();
   });
 });
+//Back-end
